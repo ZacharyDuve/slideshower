75bff73515788d9b2a3a8ff35a480cf03f466fa2
Zachary Duve <zacharyduve@Ganymede.local>
2023-01-15 23:19:06 -0500

    Hopefully fixed delay issue by having two images which the hidden one is prefetching the data before showing

--- web/index.js
+++ web/index.js
@@ -1,14 +1,45 @@
 
-function main() {
-    console.log("Main is running")
-    let imageEl = document.getElementById("mainImg");
-    getImage(imageEl);
-    window.setInterval(function(){getImage(imageEl);}, 5000);
+const delaySeconds = 5;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-//document.onload = main;
 
-function getImage(imageEl) {
+function main() {
+    //let windowHeight = window.innerHeight;
+    //let windowWidth = window.innerWidth;
+    console.log("Main is running")
+    //Get the first image ready
+    let curImage = document.getElementById("img0");
+    //image0Elm.hidden = true;
+    preloadNextImage(curImage);
+    //Load in the second image
+    let nextImage = document.getElementById("img1");
+    nextImage.hidden = true;
+    //But don't show it yet
+    preloadNextImage(nextImage);
+
+    runLoop(curImage, nextImage);
+}
+
+async function runLoop(curImage, nextImage) {
+    while(true) {
+        console.log("Swapping images");
+        //Show current image for this time
+        await sleep(delaySeconds * 1000)
+        //Swap the two images
+        curImage.hidden = true;
+        let tempImage = curImage;
+        nextImage.hidden = false;
+        curImage = nextImage;
+        nextImage = tempImage;
+        //Start loading in the next image
+        preloadNextImage(nextImage);
+    }
+}
+
+function preloadNextImage(imageEl) {
     fetch("/photo/next").then(response => response.json()).then(json => {
         console.log(json)
         let newSrc = "/photo/" + json.imageid;
@@ -18,4 +49,3 @@ function getImage(imageEl) {
 }
 
 main();
-console.log("me")
